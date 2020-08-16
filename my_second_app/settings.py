1991c4fd087521728143caca834f463ed00d1617
nahid <nahidkishore99@gmail.com>
2020-08-16 20:44:23 +0600

    Using Built-in User Model

--- my_second_app/settings.py
+++ my_second_app/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+MEDIA_DIR=os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR=[STATIC_DIR,]
+
+#MEDIA
+MEDIA_ROOT=MEDIA_DIR
+MEDIA_URL='/media/'
