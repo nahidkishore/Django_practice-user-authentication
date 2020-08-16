7adfc2768630304a93ddfcbb8937a7cd9df6b91d
nahid <nahidkishore99@gmail.com>
2020-08-17 00:40:12 +0600

    Register User

--- Login_app/views.py
+++ Login_app/views.py
@@ -8,7 +8,28 @@ def index(request):
 
 
 def register(request):
-  user_form= UserForm()
-  user_info_form= UserInfoForm()
-  dict={'user_form':user_form, 'user_info_form':user_info_form}
+  registered= False
+  
+  if request.method=='POST':
+    user_form=UserForm(data=request.POST)
+    user_info_form=UserInfoForm(data=request.POST)
+    
+    if user_form.is_valid() and user_info_form.is_valid():
+      user = user_form.save()
+      user.set_password(user.password)
+      user.save()
+      
+      user_info= user_info_form.save(commit=False)
+      user_info.user =user
+      
+      if 'profile_pic' in request.FILES:
+        user_info.profile_pic = request.FILES['profile_pic']
+        
+      user_info.save()  
+      registered=True
+        
+  else:
+    user_form= UserForm()
+    user_info_form= UserInfoForm()
+  dict={'user_form':user_form, 'user_info_form':user_info_form, 'registered':registered}
   return render(request,'Login_app/register.html', context=dict)
